fb6fc1dbdd7a85e87bd1b06ba3966287b46e6e40
Gyanu107 <gyaneshtrivedi107@gmail.com>
2024-06-20 06:34:36 +0000

    01_variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 107
+var accountEmail = "gyan@work.com"
+let accountPassword = "987456"
+accountCity = "Ahmedabad"
+
+let accountState
+
+// accountId = 1 // not allowed
+
+accountEmail = "gyanesh@work.com"
+accountPassword = "123456"
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
