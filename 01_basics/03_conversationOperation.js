971c0fe4fc64b9b6bc652f109f3b548e2861dd40
Gyanu107 <gyaneshtrivedi107@gmail.com>
2024-06-20 07:20:49 +0000

    converastion of dataTypes

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,26 @@
+
+// let score = "99" => true
+// let score = "99abc" => Nan
+// let score = null
+//let score = undefined
+//let score = true => 1; false => 0
+let score = "99"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1 true
+// let isLoggedIn = "" false
+let isLoggedIn = "Gyan" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = "18"
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
