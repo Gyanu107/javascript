3e45b34506225200ce0702ad2c86488a64c1e227
Gyanu107 <gyaneshtrivedi107@gmail.com>
2024-06-25 10:00:23 +0000

    Numbers & Maths describe

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,40 @@
+console.log('Hey Buddy!');
+
+const score = 351
+
+const balance = new Number (100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 45.95541
+
+// console.log(otherNumber.toPrecision(3));
+
+const humndreds = 210042358
+// console.log(humndreds.toLocaleString());
+// console.log(humndreds.toLocaleString('en-In'));
+
+// =================== Maths ====================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6)); // most using
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,2,9,6,1));
+// console.log(Math.max(4,3,2,9,6,1));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 17
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
